30480f3bedd42816cb4566ca64884aee92290a2d
catbugdemo <2229587083@qq.com>
2024-09-05 16:06:31 +0800

    基础配置

---
+++ gee.go
@@ -0,0 +1,48 @@
+package gee
+
+import "net/http"
+
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+// Engine implement the interface of ServeHTTP
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+// New is the constructor of gee.Engine
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+// add route and handler to the gee
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	engine.router[key] = handler
+}
+
+// GET defines the method to add GET request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+// Run defines the method to start a http server
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+// ServeHTTP implements the interface of http.Handler
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	key := r.Method + "-" + r.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, r)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		_, _ = w.Write([]byte("404 NOT FOUND: " + r.URL.Path))
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/catbugdemo/gee
+
+go 1.22.0
